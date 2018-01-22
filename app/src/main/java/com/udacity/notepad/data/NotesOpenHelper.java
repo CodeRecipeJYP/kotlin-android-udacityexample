bc020a3aedbff9406cee1d5b44a12354d42f5c9c
pjo901018 <pjo901018@korea.ac.kr>
2018-01-22 21:52:17 +0900

    Type variants / Variable decoration

--- app/src/main/java/com/udacity/notepad/data/NotesOpenHelper.java
+++ app/src/main/java/com/udacity/notepad/data/NotesOpenHelper.java
@@ -4,8 +4,6 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import static com.udacity.notepad.data.NotesContract.SQL_CREATE_ENTRIES;
-import static com.udacity.notepad.data.NotesContract.SQL_DELETE_ENTRIES;
 
 public class NotesOpenHelper extends SQLiteOpenHelper {
 
