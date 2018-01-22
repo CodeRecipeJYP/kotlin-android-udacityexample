79ca973182ab845c8def185d243fa5479c297df9
pjo901018 <pjo901018@korea.ac.kr>
2018-01-22 21:23:39 +0900

    Add data class

--- app/src/main/java/com/udacity/notepad/data/Note.kt
+++ app/src/main/java/com/udacity/notepad/data/Note.kt
@@ -2,10 +2,17 @@ package com.udacity.notepad.data
 
 import java.util.Date
 
-class Note {
-    var id = -1
-    var text: String? = null
-    var isPinned = false
-    var createdAt = Date()
+data class Note(
+    var id: Int = -1,
+    var text: String? = null,
+    var isPinned: Boolean = false,
+    var createdAt: Date = Date(),
     var updatedAt: Date? = null
+)
+
+// Playground
+fun demoDataClasses() {
+    val note = Note(text = "This is a note that I wasn't able to have used on my company project.")
+    note == note.copy()
+    note != note.copy(text = "Little changes here", isPinned = false)
 }
